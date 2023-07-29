f2149df0451e762d80e127d5fad4ac8521d48be0
Bernard Cammayo Narceda <bnarceda.04@gmail.com>
2023-07-29 14:23:53 +0800

    Added HomeScreen and RouteCards

---
+++ TODA/app/src/main/java/com/znapp/toda/screens/HomeScreen.kt
@@ -0,0 +1,91 @@
+package com.znapp.toda.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.znapp.toda.R
+import com.znapp.toda.ui.composables.RoundedButtonEmail
+import com.znapp.toda.ui.composables.RoundedButtonGoogle
+import com.znapp.toda.ui.composables.RoundedButtonTODA
+import com.znapp.toda.ui.theme.Montserrat
+import com.znapp.toda.ui.theme.TODATheme
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = with(Modifier) {
+            fillMaxSize()
+            paint(
+                painterResource(id = R.drawable.toda_welcome_bg),
+                contentScale = ContentScale.FillBounds
+            )
+        })
+    {}
+    TODATheme {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Spacer(modifier = Modifier.height(290.dp)
+            )
+
+            Text(
+                text = "Welcome to TODA",
+                color = Color.White,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Text(
+                text = "Log in with Google to continue",
+                color = Color.White,
+                fontFamily = Montserrat,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(15.dp)
+            )
+
+            RoundedButtonGoogle(text = " Continue with Google") {
+            }
+
+            Spacer(modifier = Modifier.height(170.dp)
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.dev_by_znapp),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
---
+++ TODA/app/src/main/java/com/znapp/toda/ui/composables/RouteCards.kt
@@ -0,0 +1,111 @@
+package com.znapp.toda.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CardElevationCaloocan1() {
+    Surface(
+        shape = RoundedCornerShape(16.dp),
+        color = Color(0xFFDAE1E7),
+        modifier = Modifier
+            .height(210.dp)
+            .padding(10.dp),
+        shadowElevation = 10.dp
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Surface(
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier.wrapContentSize(),
+                    color = Color(0xFFD1D5E1)
+                ) {
+                    Text(
+                        text = "New release",
+                        fontSize =  12.sp,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = "Gift Plant",
+                    fontSize =  24.sp,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.SemiBold
+                )
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Text(text = "Price : 300$")
+
+                Spacer(modifier = Modifier.height(2.dp))
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = "4.0",
+                        fontSize =  14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                OutlinedButton(
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = Color.Black,
+                        containerColor = Color.White
+                    ),
+                    onClick = { /*TODO*/ }
+                ) {
+                    Text(
+                        text = "Read More",
+                        fontSize =  11.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardElevationCaloocan1Preview(){
+    CardElevationCaloocan1()
+}
